27f5cc0e50c1aa34d016b1f64b40995e11781b89
ompangyji <ompangyji@gmail.com>
2023-10-05 16:58:19 +0900

    6242_loop6 update

--- 6242_loop6/sol.py
+++ 6242_loop6/sol.py
@@ -1,8 +1,21 @@
-bloodtype = ['A', 'A', 'A', 'O', 'B', 'B', 'O', 'AB', 'AB', 'O', 'C']
-# dic_blood = {'A': 0, 'O': 0, 'B': 0, 'AB': 0}
+from collections import Counter
 
-# for blood in dic_blood.keys():
+blood_list = ['A', 'A', 'A', 'O', 'B', 'B', 'O', 'AB', 'AB', 'O', 'C']
+# blood_dict = {'A': 0, 'O': 0, 'B': 0, 'AB': 0}
+
+# for blood in blood_dict.keys():
     
-#     dic_blood[blood] = bloodtype.count(blood)
+#     blood_dict[blood] = blood_list.count(blood)
 
-# print(dic_blood)
+# print(blood_dict)
+
+#===========================================
+
+blood_dict = {}
+counts = Counter(blood_list)
+#print(blood_dict)    #<- Counter({'A': 3, 'O': 3, 'B': 2, 'AB': 2, 'C': 1})
+
+for element, count in counts.items():
+    blood_dict[element] = count
+
+print(blood_dict)
